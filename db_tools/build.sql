c129bb783d551a6027ce994d1ce390e8884edb6a
Dario Pagani <dario.pagani@itispisa.gov.it>
2018-01-17 18:38:43 +0100

    First commiti

---
+++ db_tools/build.sql
@@ -0,0 +1,13 @@
+/*CREATE DATABASE tirocini;
+
+USE tirocini;*/
+
+CREATE TABLE Indirizzi
+(
+  id                              INT UNSIGNED AUTO_INCREMENT,
+  nome                            VARCHAR(255),
+
+  PRIMARY KEY (id)
+);
+
+
---
+++ test/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dario
+ * Date: 27/12/17
+ * Time: 19.59
+ */
+?>
