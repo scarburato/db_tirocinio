7bb04286d950060a6674c7525cfb02c360efc1b7
Dario Pagani <dario.pagani@itispisa.gov.it>
2018-03-02 19:02:09 +0100

    Era impossibile commentare più volte -.-

--- db_tools/build.sql
+++ db_tools/build.sql
@@ -183,7 +183,7 @@ CREATE TABLE IF NOT EXISTS Commento (
   testo     TEXT      NOT NULL,
   quando    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 
-  UNIQUE (tirocinio, autore),
+  UNIQUE (tirocinio, autore, quando),
 
   FOREIGN KEY (Tirocinio)
   REFERENCES Tirocinio (id),
