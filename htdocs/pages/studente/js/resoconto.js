706763d8353f7df535f05009978a28a733cb15e3
Dario Pagani <dario.pagani@itispisa.gov.it>
2018-03-05 15:44:15 +0100

    Operatori ugualianza errati, formatazzione errata

--- htdocs/pages/studente/js/resoconto.js
+++ htdocs/pages/studente/js/resoconto.js
@@ -20,13 +20,15 @@ let x = new ToggleTab ($ ("#selector"), $ ("#contents"), PASSED);
 // TODO implementare visualizzazione dinamica di commenti
 x.onChange (function (e)
 {
-	if (e=="preview" && editor!==undefined) {
+	if (e === "preview" && editor !== undefined)
+	{
 		$ ("#preview_editor").html (
 			editor.val ()
 		);
 	}
-	if (e=="comments" && PASSED!="comments") {
-		window.location.href = window.location.href+"&page=comments";
+	if (e === "comments" && PASSED !== "comments")
+	{
+		window.location.href = window.location.href + "&page=comments";
 		//newMSG = false;
 	}
 });
@@ -34,45 +36,52 @@ x.onChange (function (e)
 // Bottoni
 // Bottone per salvare modifiche alla descrizione
 $ ("#bt_save").on ("click", function ()
-{
-	let temp = $("#preview_editor").html();
-	if ($.md5(temp)!=md5_ATT) {
-		if (confirm("Continuando modificherà la descrizione e non potrà risalire al suo precedente valore!")) {
-			$.post(
-				BASE+'rest/trainings/update_Descrizione.php', {contenuto: temp, tirocinio: TIR}
-			).done( function(data) {
-				md5_ATT=data.md5;
-			});
+	{
+		let temp = $ ("#preview_editor").html ();
+		if ($.md5 (temp) !== md5_ATT)
+		{
+			if (confirm ("Continuando modificherà la descrizione e non potrà risalire al suo precedente valore!"))
+			{
+				$.post (
+					BASE + 'rest/trainings/update_Descrizione.php', {contenuto: temp, tirocinio: TIR}
+				).done (function (data)
+				{
+					md5_ATT = data.md5;
+				});
+			}
 		}
 	}
-}
 );
 
 // Per evitare lo spam di commenti
-let nonPigiareTroppo=false;
+let nonPigiareTroppo = false;
 //let newMSG = false;
-window.setInterval(function() {
-	nonPigiareTroppo=false;
+window.setInterval (function ()
+{
+	nonPigiareTroppo = false;
 }, 1000);
 
-$("#bt_comments").on("click", function () {
+$ ("#bt_comments").on ("click", function ()
+{
 	if (nonPigiareTroppo)
 		alert ("aspetta un secondo prima di inviare un nuovo commento!");
 	nonPigiareTroppo = true;
-	let comment = $("#commento").val();
-	if (comment=="" || comment==undefined || comment==null)
+	let comment = $ ("#commento").val ();
+	if (comment === "" || comment === undefined || comment === null)
 		return;
-	$.post(
-		BASE+'rest/trainings/commenta.php', {contenuto: comment, tirocinio: TIR}
-	).done(function () {
-		window.location.reload();
+	$.post (
+		BASE + 'rest/trainings/commenta.php', {contenuto: comment, tirocinio: TIR}
+	).done (function ()
+	{
+		window.location.reload ();
 		//newMSG = false;
-	})/*.fail(function () {
-		console.log("errore per la post!?!");
-	}).always(function(data){
-		console.log("at least it's something...");
-		console.log(data);
-	});*/
+	})
+	/*.fail(function () {
+			console.log("errore per la post!?!");
+		}).always(function(data){
+			console.log("at least it's something...");
+			console.log(data);
+		});*/
 });
 
 /* TODO implementare questo intervallo o prendere ed implementare i pesanti WEB SOCKET
