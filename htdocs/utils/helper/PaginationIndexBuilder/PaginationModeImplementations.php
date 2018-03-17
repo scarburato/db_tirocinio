1961ae9a3b998ee9cf9925a28a9bd9085936746f
Dario Pagani <dario.pagani@itispisa.gov.it>
2018-03-17 19:10:42 +0100

    Rifattorizato generazione indice
    - Creata una interfaccia per gestire la creazione dell'indice
        > Creata una classe che implementanta la suddetta in modo che funzioni con HTTP e che abbia un comportamento equivalente a quello precedente
        > Creata una classe che implementa la suddetta in modo da potter essere sfruttata con facilità per la creazione di pagine dinamiche con AJAX e JS
    
    - Creata una classe per la gestione degli attributi in HTML, potrebbe tornare utile anche in futuro. La classe un funzionamento accettabile anche se andrebbe rivista per alcune piccolezze

---
+++ htdocs/utils/helper/PaginationIndexBuilder/PaginationModeImplementations.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dario
+ * Date: 17/03/18
+ * Time: 18.29
+ */
+
+namespace helper;
+
+class IndexHTTP implements PaginationMode
+{
+    private $opt_param = [];
+
+    private function build_query(int $page): string
+    {
+        $query = "";
+        if(count($this->opt_param) > 0)
+            $query = "&" . http_build_query($this->opt_param);
+
+        return "?pagina={$page}" . $query;
+    }
+
+    /**
+     * Sono i paramatri GET da salvarsi!
+     * @param array $opt_param
+     */
+    public function set_opt_param(array $opt_param): void
+    {
+        unset($opt_param["pagina"]);
+        $this->opt_param = $opt_param;
+    }
+
+    public function offset_page(Pagination $current, int $offset): Attributes
+    {
+        return new Attributes(
+            [
+                "href" => [$this->build_query($current->get_current_page() + $offset)]
+            ]
+        );
+    }
+
+    public function last_page(Pagination $current): Attributes
+    {
+        return new Attributes(
+            [
+                "href" => [$this->build_query($current->get_max_page())]
+            ]
+        );
+    }
+
+    public function first_page(Pagination $current): Attributes
+    {
+        return new Attributes(
+            [
+                "href" => [$this->build_query(0)]
+            ]
+        );
+    }
+
+    public function next_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, +1);
+    }
+
+    public function previus_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, -1);
+
+    }
+
+    public function current_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, 0);
+    }
+}
+
+class IndexJS implements PaginationMode
+{
+    public function offset_page(Pagination $current, int $offset): Attributes
+    {
+        return new Attributes(
+            [
+                "data-page" => [$current->get_current_page() + $offset]
+            ]
+        );
+    }
+
+    public function last_page(Pagination $current): Attributes
+    {
+        return new Attributes(
+            [
+                "data-page" => [$current->get_max_page()]
+            ]
+        );
+    }
+
+    public function first_page(Pagination $current): Attributes
+    {
+        return new Attributes(
+            [
+                "data-page" => [0]
+            ]
+        );
+    }
+
+    public function next_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, +1);
+    }
+
+    public function current_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, 0);
+    }
+
+    public function previus_page(Pagination $current): Attributes
+    {
+        return $this->offset_page($current, -1);
+    }
+}
